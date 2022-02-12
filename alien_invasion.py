1e33da454bb94cc8448936f170259c35f874f09f
Marcus Ventin <94906498+marcusventin@users.noreply.github.com>
2022-02-12 13:39:53 +0000

    Added move right functionality

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,15 +25,21 @@ class AlienInvasion:
         """Start the game's main loop."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
-           
-    
     def _check_events(self):
         """Respond to keypresses and mouse events."""
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+
     
     def _update_screen(self):
         """Update images on the screen and flip to the new screen."""
@@ -41,7 +47,7 @@ class AlienInvasion:
         self.ship.blitme()
 
         # Make the most recently drawn screen visible.
-        
+
         pygame.display.flip()
 
 if __name__ == '__main__':
