5f3147fe0f21557bdd633f6d148acec63f2a0252
Marcus Ventin <94906498+marcusventin@users.noreply.github.com>
2022-02-13 13:07:24 +0000

    Can shoot aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -34,6 +34,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
 
             self._update_screen()
 
@@ -81,6 +82,16 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+        
+        # Check for any bullets that have hit aliens.
+        # If so, get rid of the bullet and the alien.
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True)
+    
+    def _update_aliens(self):
+        """Update the position of all aliens in the fleet."""
+        self._check_fleet_edges()
+        self.aliens.update()
     
     def _create_fleet(self):
         """Create the fleet of aliens."""
@@ -110,6 +121,19 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         """Update images on the screen and flip to the new screen."""
