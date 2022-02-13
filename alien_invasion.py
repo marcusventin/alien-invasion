ed6b044c350d118decddb1634c15e352495e21cd
Marcus Ventin <94906498+marcusventin@users.noreply.github.com>
2022-02-13 13:15:38 +0000

    Added collision detection for aliens and ships

--- alien_invasion.py
+++ alien_invasion.py
@@ -83,15 +83,27 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
         
-        # Check for any bullets that have hit aliens.
-        # If so, get rid of the bullet and the alien.
+        self._check_bullet_alien_collisions()
+        
+    def _check_bullet_alien_collisions(self):
+        """Respond to bullet-alien collisions."""
+        # Remove any bullets and aliens that have collided.
         collisions = pygame.sprite.groupcollide(
             self.bullets, self.aliens, True, True)
-    
+        
+        if not self.aliens:
+            # Destroy existing bullets and create new fleet:
+            self.bullets.empty()
+            self._create_fleet()
+
     def _update_aliens(self):
         """Update the position of all aliens in the fleet."""
         self._check_fleet_edges()
         self.aliens.update()
+
+        # Look for alien-ship collisions.
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            print("Ship hit!!!")
     
     def _create_fleet(self):
         """Create the fleet of aliens."""
